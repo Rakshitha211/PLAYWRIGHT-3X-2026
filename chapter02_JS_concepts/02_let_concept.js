ac0860217758171fd350bbc07c8ff2a479ae4fdb
Rakshitha <rakshitha@example.com>
2026-08-01 07:20:44 +0530

    Add latest JavaScript practice updates

---
+++ chapter02_JS_concepts/02_let_concept.js
@@ -0,0 +1,12 @@
+
+//console.log(y);
+//let y = 10;
+
+console.log('5' - 3);
+
+console.log(0 ?? 'hello');
+console.log(0 || 'hello');
+
+console.log(Boolean([]));
+
+for (let i = 0; i < 5; i++) { if (i === 3) break; } console.log(i);
